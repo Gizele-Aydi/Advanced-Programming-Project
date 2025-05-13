adc4e026320eafd20b11ef1a3b078f16ad4fe025
Gisèle <aydigizele@tbs.u-tunis.tn>
2025-05-13 22:18:50 +0100

    sign in ui

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -94,7 +94,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
     implementation(libs.androidx.navigation.fragment)
 
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
+
+
