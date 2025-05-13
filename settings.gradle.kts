e2f1a4fc2a3ae5adc2813c0ce71f66850b1f5221
adem-maatallah <maatallah.adem8@gmail.com>
2025-05-13 23:57:01 +0100

    Version Compatibility issue

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,25 @@ pluginManagement {
         mavenCentral()
         gradlePluginPortal()
     }
+
+    // <-- Add this:
+    resolutionStrategy {
+        eachPlugin {
+            when (requested.id.id) {
+                // maps alias(libs.plugins.android.application)
+                "com.android.application" ->
+                    useModule("com.android.tools.build:gradle:${requested.version}")
+                // maps alias(libs.plugins.kotlin.android)
+                "org.jetbrains.kotlin.android" ->
+                    useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:${requested.version}")
+                // maps alias(libs.plugins.google.services)
+                "com.google.gms.google-services" ->
+                    useModule("com.google.gms:google-services:${requested.version}")
+            }
+        }
+    }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -21,4 +39,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "Moodify"
 include(":app")
- 
